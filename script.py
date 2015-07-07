52173e09b13ae0687feb19d471409b55d2f49e29
kamil <kamil4892@interia.pl>
2015-07-07 03:07:32 +0200

    mostly GUI class updated

--- script.py
+++ script.py
@@ -2,6 +2,12 @@
  @author ILoveMuffins
 '''
 
+# wyswietl 5 najlepszych wynikow z pliku/bazy danych
+# wraz z nick'ami graczy
+# wyswietl autora
+# wcisnij <select> aby wybrac gracza
+# if self.player != None: "wcisnij <start> aby zagrac"
+
 import psp2d, pspos
 import random
 from time import time
@@ -45,59 +51,58 @@ class Player:
 
 class Logic:
     def __init__(self):
-        pass
-        self.int_to_button = {0:Left(), 1:Right(), 2:Up(), 3:Down(),
+        self.int_to_button = { 0:Left(), 1:Right(), 2:Up(), 3:Down(),
                4:Triangle(), 5:Square(), 6:Cross(), 7:Circle() }
+        self.MAX_WAITING_TIME_FOR_BUTTON_APPEAR_SEC = 5
+        self.MAX_SHOWING_FREQENCY = 25
 
     def generate_button(self):
         rand_int = random.randint(0, 7)
-        self.rand_button = self.int_to_button[rand_int]
+        rand_button = self.int_to_button[rand_int]
+        return rand_button
 
-    def check_button(self, button):
-        return self.button == button
+    def compute_time_to_wait_for_button_appear(self):
+        rand = random.randint(0, self.MAX_SHOWING_FREQENCY)
+        time_to_wait_for_button_appear = \
+            (self.MAX_WAITING_TIME_FOR_BUTTON_APPEAR_SEC * rand) \
+            / self.MAX_SHOWING_FREQENCY
+        return time_to_wait_for_button_appear
 
-# Time, Player, Logic, GUI
-# @TODO wszystko
+    def compute_viewing_time(self):
+        pass #@TODO
+
 class GUI:
     def __init__(self):
-        self.screen = psp2d.Screen()
-        color = psp2d.Color(0,0,0,255)
-        self.screen.clear(color)
+        self.initialize_screen()
         self.font = psp2d.Font('font.png')
-        self.player = None #@TODO
+        self.player = Player('Zuitek')
         self.logic = Logic()
-        self.menu_options = { 0:('Start',220), 1:('High Score',235),
-                    2:('Exit',250) }
+        self.menu_options = { 0:('Start',220, start_game),
+                1:('High Score',235, high_score), 2:('Exit',250, exit) }
         self.OPTIONS_NUMBER = len(self.menu_options)
         self.marked_option = 0
 
+    def initialize_screen(self):
+        self.screen = psp2d.Screen()
+        self._clear_screen()
+
+    def _clear_screen(self):
+        color = psp2d.Color(0,0,0,255)
+        self.screen.clear(color)
+
     def run(self):
-        self.menu()
-
-    def menu(self):
         self._draw_menu()
-        # wyswietl 5 najlepszych wynikow z pliku
-        # wraz z nick'ami graczy
-        # wyswietl autora
-        # wcisnij <select> aby wybrac gracza
-        # if self.player != None: "wcisnij <start> aby zagrac"
-
-        # @TODO opakowac w klase, uzyc w programie
-        pad = psp2d.Controller()
-        if pad.start:
-            gui.start_game()
-
-        pass
+        self._get_chosen_menu_option()
+        reaction_function = self.menu_options[self.marked_option][3]
+        reaction_function()
 
     def _draw_menu(self):
-        self._draw_mark_rect(100, self.menu_options[self.marked_option][2])
+        self._draw_mark_rect_in_point(100, self.menu_options[self.marked_option][2])
         self._print_menu_options()
-        pad = psp2d.Controller()
-        _menu_loop(self)
 
     def _draw_mark_rect_in_point(self, x, y):
         color = psp2d.Color(0, 200, 0, 255)
-        rectangle = Rect(x, y, 220, 15)
+        rectangle = Rect(x, y, width=220, height=15)
         rect(self.screen, color, rectangle)
 
     def _print_menu_options(self):
@@ -105,16 +110,53 @@ class GUI:
         font.drawText(self.screen, 180, 240, self.menu_options[1][0])
         font.drawText(self.screen, 180, 255, self.menu_options[2][0])
 
-    def _menu_loop(self):
+    def _get_chosen_menu_option(self):
+        pad = psp2d.Controller()
         while not pad.start:
-            if pad.up:
-                self.marked_option = abs(self.marked_option - 1) % 3
-            elif pad.down:
-                self.marked_option = self.marked_option + 1 % 3
+            _react_to_pad_event_in_menu(pad)
 
+    def _react_to_pad_event_in_menu(self, pad):
+        if pad.up:
+            self._move_to_prev_option()
+        elif pad.down:
+            self._move_to_next_option()
+
+    def _move_to_next_option(self):
+        self.marked_option = (self.marked_option + 1) % 3
+
+    def _move_to_prev_option(self):
+        self.marked_option -= 1
+        if self.marked_option < 0:
+            self.marked_option = self.OPTIONS_NUMBER
+
+#player, logic, time
     def start_game(self):
-        sleep(2)
+        sleep(1)
+        while not self.player.has_all_points():
+            sleep(1)
+            self._clear_screen()
+            button = self.logic.generate_button()
+            waiting_time = self.logic.compute_time_to_wait_for_button_appear()
+            view_time = self.logic.compute_viewing_time()
+            sleep(waiting_time)
+            #@TODO
+            # narysuj teksture przechowywana przez button
+            # wystartuj watek czekajacy na wejscie, wystartuj pomiar czasu
+            # jak tylko wejscie sie pojawi zatrzymaj pomiar, usun obrazek
+            sleep(view_time)
+            # zatrzymaj pomiar czasu (jesli watek jeszcze go nie zatrzymal)
+            # usun obrazek
+            # oblicz punkty dla gracza
+            # wywolaj odpowiednie metody z API gracza
+
+    def high_score(self):
         pass
+
+    def exit(self):
+        # for debug - red screen after 'exit'
+        color = psp2d.Color(255,0,0,255)
+        self.screen.clear(color)
+        sleep(2)
 
 gui = GUI()
 gui.run()
