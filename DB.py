43a5f0944a1f232bca578bab80487522d652c139
kamil <kamil4892@interia.pl>
2015-07-09 14:41:09 +0200

    reverse database

--- DB.py
+++ DB.py
@@ -7,11 +7,9 @@ class DB:
         self.cursor.execute("INSERT INTO Scores (Nick, CorrectAns, AllAns, Score) VALUES ('bonek', 0, 10, 1)")
         self.connection.commit()
         self.connection.close()
-
     def p(self):
         self.connection = sqlite3.connect('scores.db')
         self.cursor = connection.cursor()
         for row in self.cursor.execute('SELECT * FROM scores.db ORDER BY CorrectAns, Score'):
             print row
         self.connection.close()
-
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # reflex-game
 Small PSP game in Python, working on your reflex
 You need Stackless Python installed on your PSP to run this app
-Before copying app to PSP, run createDB.py script
